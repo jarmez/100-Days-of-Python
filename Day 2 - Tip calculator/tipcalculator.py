8ca3e9b1e7ef0bab396688a867f115e656924335
James Day <james@day.net.nz>
2025-10-23 10:05:12 +1300

    iniitial commit

---
+++ Day 2 - Tip calculator/tipcalculator.py
@@ -0,0 +1,12 @@
+# Soultion shows two ways of diong this, final statement calculating bill in one statement
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+people = int(input("How many people to split the bill? "))
+# tip_as_percentage = tip /100
+# tip_total = bill * tip_as_percentage
+# bill_total = bill + tip_total
+# bill_per_person = bill_total / people
+# final_amount = round(bill_per_person, 2)
+# print(f"Each person should pay: ${final_amount}")
+print("Each person should pay: " + str(round((bill * (1 + (tip / 100)) / people), 2)))
---
+++ Day 4 - Rock Paper Scissors/rockpaperscissors.py
@@ -0,0 +1,78 @@
+# Rock Paper Scissors
+import random
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+player_choice = int(input("What do you choose? Type 0 for Rock, Type 1 for Paper, or 2 for Scissors.\n"))
+if 0 <= player_choice <= 2:
+    computer_choice = random.randint(0,2)
+    items = [rock, paper, scissors]
+    print("You chose . . . " + items[player_choice])
+    print("The computer chose . . . " + items[computer_choice])
+    if items[player_choice] == rock:
+        if items[computer_choice] == rock:
+            print("It's a draw!")
+        elif items[computer_choice] == paper:
+            print("You loose! Paper beats Rock.")
+        elif items[computer_choice] == scissors:
+            print("You win! Rock beats Scissors")
+    elif items[player_choice] == scissors:
+        if items[computer_choice] == rock:
+            print("You lose! Rock beats Scissors")
+        elif items[computer_choice] == paper:
+            print("You win! Scissors beats Paper.")
+        elif items[computer_choice] == scissors:
+            print("It's a draw")
+    elif items[player_choice] == paper:
+        if items[computer_choice] == rock:
+            print("You win! Paper beats Rock.")
+        elif items[computer_choice] == paper:
+            print("It's a draw!")
+        elif items[computer_choice] == scissors:
+            print("You lose! Scissors beats Paper.")
+else:
+    print("Invalid entry. Game over")
+
+
+#   Alternative Logic from course
+#
+# computer_choice = random.randint(0,2)
+# if player_choice == 0 and computer_choice == 2:
+#     print("You win!")
+# if computer_choice == 0 and player_choice == 2:
+#     print("You lose!")
+# elif computer_choice > player_choice:
+#     print("You lose!")
+# elif computer_choice < player_choice:
+#     print("You win!")
+# elif player_choice == computer_choice:
+#     print("It's a draw.")
+# else:
+#     print("You typed an invalid number")
+
+
+
