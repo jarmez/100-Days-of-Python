01daaa7f241ce19404065c34189584f48350a593
James Day <james@day.net.nz>
2026-02-16 21:24:48 +1300

    Day one - Band Name generator

---
+++ Day 1 - Band name generator/bandnamegenerator.py
@@ -0,0 +1,5 @@
+#!/bin/python
+print("Welcome to the Band Name Generator!")
+city = input("What is the city you grew up in? ")
+pet = input("What is the name of your pet? ")
+print("Your band name could be: " + city + " " + pet)
