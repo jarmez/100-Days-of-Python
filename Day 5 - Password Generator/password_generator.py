4ea1a2f3025b9b186244bbaa414a49dedcf66194
James Day <james@day.net.nz>
2026-08-30 20:12:50 +1200

    Commit changes

--- Day 5 - Password Generator/password_generator.py
+++ Day 5 - Password Generator/password_generator.py
@@ -1,8 +1,63 @@
 #!/bin/python
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+import random
+
+letters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+]
+numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+symbols = ["!", "#", "$", "%", "&", "(", ")", "*", "+"]
 
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
@@ -10,7 +65,6 @@ nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 total = nr_letters + nr_symbols + nr_numbers
 
-import random
 password = ""
 for i in range(0, nr_letters):
     password += random.choice(letters)
@@ -21,7 +75,7 @@ for i in range(0, nr_numbers):
 
 char_list = list(password)
 random.shuffle(char_list)
-#randomized_password = "".join(char_list)
+# randomized_password = "".join(char_list)
 randomized_password = ""
 for i in char_list:
     randomized_password += i
@@ -30,6 +84,3 @@ for i in char_list:
 # randomized_password = "".join(char_list)
 
 print(randomized_password)
-
-
-
